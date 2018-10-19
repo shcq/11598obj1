5031ef40493daf2526394f7366e038114eef1449
1269483752 <1269483752@qq.com>
2018-10-19 17:53:37 +0800

    更新-wu

--- index.php
+++ index.php
@@ -1,3 +1,36 @@
+<?php
+	$mydb=new mysqli('localhost','root','root','weblog',3306);
+	$mydb->query('SET NAMES UTF8');
+	
+	if(isset($_GET['arid'])){
+		$arid=$_GET['arid'];
+		$sql='SELECT arid ,title ,  content FROM arlist WHERE status = 1 AND arid=' .$arid;
+		
+		$r=$mydb->query($sql);
+		$list=$r->fetch_all(MYSQLI_ASSOC);
+	}else{
+		$sql='SELECT arid ,title ,  content FROM arlist WHERE status = 1 LIMIT 5';
+		$r=$mydb->query($sql);
+		$list=$r->fetch_all(MYSQLI_ASSOC);
+	}
+	
+	
+	
+	//查询热门文章
+	$sql='SELECT arid, arnums,title ,content FROM arlist WHERE status = 1  order by arnums desc LIMIT 5';
+	$x=$mydb->query($sql);
+	$hot=$x->fetch_all(MYSQLI_ASSOC);
+	
+	//查询最近访问用户
+	$sql='SELECT cuid,cuname,cunums FROM currentuser WHERE 1 order by time desc LIMIT 5';
+	$y=$mydb->query($sql);
+	$currentuser=$y->fetch_all(MYSQLI_ASSOC);
+	
+	
+//	var_dump($list);
+//	exit;
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -29,7 +62,7 @@
                 </div>
                 <div class="row text-center nav_h" style="height: 50px;line-height: 50px">
                     <div class="col-2"><a href="index.php">首页</a></div>
-                    <div class="col-2"><a href="hotarticle.php">热门文章</a></div>
+                    <div class="col-2"><a href="index.php?hotarticle=hotarticle">热门文章</a></div>
                     <div class="col-2"><a href="leavemessage.php">留言</a></div>
                     <div class="col-2"><a href="aboutus.php">关于我们</a></div>
                 </div>
@@ -57,7 +90,7 @@
     </div>
     <!--内容主题-->
     <div class="container">
-        <div class="row">
+        <div class="row" style="overflow: hidden;">
             <!--左侧各个功能框-->
             <div class="col-3"  style="padding-left: 0 ">
                 <!--博主信息简介-->
@@ -82,26 +115,93 @@
                 </div>
 
                 <!--最近访问用户-->
-                <div style="height: 200px;border: 1px solid #c5b164;margin-top: 10px;" class="model_bg">
+                <div class="model_bg">
                     <div style="width: 100%;height:50px;text-align: center;line-height: 50px">最近访问的用户</div>
-                    <div style="width: 200px;height:130px;margin:5px  auto;border: 1px solid #c5b164">
-                        hhh
+                    <div style="width: 200px;height:250px;margin:5px  auto;border: 1px solid #c5b164">
+                    	<ul class="user">
+							<?php
+							foreach($currentuser as $k=>$v){
+//						var_dump($v);
+//						exit;
+						echo '<li>'.$v['cuname'].' <span>第'.$v['cunums'].'次访问</span></li>';
+							}
+						?>
+						</ul>	
                     </div>
                 </div>
 
                 <!--热门文章-->
-                <div style="height: 200px;border: 1px solid #c5b164;margin-top: 10px" class="model_bg">
+                <div class="model_bg">
                     <div style="width: 100%;height:50px;text-align: center;line-height: 50px">热门文章</div>
-                    <div style="width: 200px;height:130px;margin:5px  auto;border: 1px solid #c5b164">
-                         hhh
+                    <div style="width: 200px;height:250px;margin:5px  auto;border: 1px solid #c5b164">
+                        <ul class="hot">
+                    		<?php
+                    		foreach($hot as $k=>$v){
+                    	//	var_dump($v);
+                    	//	exit;
+                    		echo '<li>
+                    				<a href="index.php?arid='.$v['arid'].'">
+                    				 '.$v['title'].' </a><span> '.$v['arnums'].'次浏览</span>
+                    			  </li>';
+                    			}
+                    		?>
+                    	</ul>
                     </div>
                 </div>
             </div>
 
             <!--右边文章主体-->
             <div class="col-9" class="model_bg">
-                <div style="width: 100%;height: 100px;text-align: center;line-height: 100px"><span class="font">发表的文章</span></div>
+                <div style="width: 100%;height: 100px; text-align: center;line-height: 100px"><span class="font">发表的文章</span></div>
                 <div style="width: 100%;height: 3px;background: #c5b164"></div>
+                <div class="articlebox" style=" overflow: hidden;">
+                	<?php
+                		//文章详情
+                		if(isset($_GET['arid'])){
+                			foreach($list as $k=>$v){
+                	//			var_dump($v);
+                	//			exit;
+                			echo '
+                				<div class="article">
+                				<div style="magin-top:20px;"><h3> '.$v['title'].'</h3></div>
+                				<hr>
+                				<div class="content1"> '.$v['content'].' </div>
+                				</div>
+                				';
+                		}
+                			//热门文章列表，按浏览次数来排序
+                		}else if(isset($_GET['hotarticle'])){
+                				foreach($hot as $k=>$v){
+                	//			var_dump($v);
+                	//			exit;
+                			echo '
+                				<div class="article">
+                				<h4><a href="index.php?arid='.$v['arid'].'"> '.$v['title'].' </a><div  style="text-align:right;padding-right:180px;font-size:13px"> <b>'.$v['arnums'].'</b>次浏览</div></h4>
+                				<hr>
+                				<div class="content"> '.$v['content'].' </div>
+                				</div>
+                				';
+                		}
+                		}
+                		
+                		
+                		//文章列表
+                		else{
+                				foreach($list as $k=>$v){
+                	//			var_dump($v);
+                	//			exit;
+                			echo '
+                				<div class="article">
+                				<h4><a href="index.php?arid='.$v['arid'].'"> '.$v['title'].' </a></h4>
+                				<hr>
+                				<div class="content"> '.$v['content'].' </div>
+                				</div>
+                				';
+                		}
+                		
+                		}
+                	?>
+                </div>
             </div>
         </div>
     </div>
